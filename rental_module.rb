09a1b169581edd10ca3e435fad58c54f5ec35775
faizi2500 <faizan.zahid2500@gmail.com>
2021-11-16 13:30:04 +0500

    Refactor module to make code follow SP principle

--- rental_module.rb
+++ rental_module.rb
@@ -2,16 +2,29 @@ require './rental'
 require './classroom'
 
 module NewRental
-  def create_rentals
-    print "Select a book from the folowing list by number\n"
+  def extract_books
     @books.each_with_index { |book, index| puts "#{index}) Title: #{book.title}, Author: #{book.author}" }
-    book_index = gets.chomp.to_i
-    print "Select a person from the folowing list by number (not id)\n"
+    gets.chomp.to_i
+  end
+
+  def extract_persons
     @people.each_with_index { |p, index| puts "#{index}) [#{p.class}] Name: #{p.name}, Age: #{p.age}, ID: #{p.id}" }
-    person_index = gets.chomp.to_i
+    gets.chomp.to_i
+  end
+
+  def extract_rental_info
+    print "Select a book from the folowing list by number\n"
+    book_selected = extract_books
+    print "Select a person from the folowing list by number (not id)\n"
+    person_rented = extract_persons
+    [book_selected, person_rented]
+  end
+
+  def create_rentals
+    book_selected, person_rented = extract_rental_info
     print 'Date: '
     date = gets.chomp
-    rental = Rental.new(date, @books[book_index], @people[person_index])
+    rental = Rental.new(date, @books[book_selected], @people[person_rented])
     @rentals << rental
     puts 'Rental created successfully'
   end
